3bff52f9552faa29da815189c94f2756cf054b46
Pham Truong <18521571@gm.uit.edu.vn>
2021-07-18 00:15:46 +0700

    play

--- editor/js/Menubar.js
+++ editor/js/Menubar.js
@@ -6,7 +6,7 @@ import { MenubarFile } from './Menubar.File.js';
 // import { MenubarExamples } from './Menubar.Examples.js';
 import { MenubarView } from './Menubar.View.js';
 // import { MenubarHelp } from './Menubar.Help.js';
-// import { MenubarPlay } from './Menubar.Play.js';
+import { MenubarPlay } from './Menubar.Play.js';
 import { MenubarStatus } from './Menubar.Status.js';
 
 function Menubar( editor ) {
@@ -17,7 +17,7 @@ function Menubar( editor ) {
 	container.add( new MenubarFile( editor ) );
 	container.add( new MenubarEdit( editor ) );
 	container.add( new MenubarAdd( editor ) );
-	// container.add( new MenubarPlay( editor ) );
+	container.add( new MenubarPlay( editor ) );
 	// container.add( new MenubarExamples( editor ) );
 	container.add( new MenubarView( editor ) );
 	// container.add( new MenubarHelp( editor ) );
