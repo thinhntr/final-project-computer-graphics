2d12da2151475513fc34d0fe5006428f40922c0c
Thinh Nguyen <thinhntr@gmail.com>
2021-07-18 19:50:43 +0700

    Update background for light theme

--- app/js/Menubar.Status.js
+++ app/js/Menubar.Status.js
@@ -1,11 +1,28 @@
+import * as THREE from "./three.module.js";
+
 import { UIPanel } from './libs/ui.js';
 import { UIBoolean } from './libs/ui.three.js';
 
-function Checkbox(value) {
+function Checkbox(value, editor) {
+	const signals = editor.signals;
 	const body = document.body;
 	if(value) {
+		signals.sceneBackgroundChanged.dispatch(
+      'Color',
+      0x171717,
+      '',
+      '',
+      ''
+    );
 		body.classList.add('dark-theme');
 	} else {
+		signals.sceneBackgroundChanged.dispatch(
+      'Color',
+      0xAAAAAA,
+      '',
+      '',
+      ''
+    );
 		body.classList.remove('dark-theme');
 	};
 }
@@ -22,7 +39,7 @@ function MenubarStatus( editor ) {
 	darkmode.text.setColor( '#888' );
 	darkmode.onChange( function () {
 		var value = this.getValue();
-		Checkbox(value);
+		Checkbox(value, editor);
 	} );
 	container.add( darkmode );
 
